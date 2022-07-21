463ecb359a95a0a47a05542b4a0d565a24968850
Taehoon Jang <cloudlesslakitu67@gmail.com>
2022-07-21 17:45:34 +0900

    Develop core function

---
+++ src/NpmScriptsProvider.ts
@@ -0,0 +1,49 @@
+import {
+  TreeDataProvider,
+  TreeItem,
+  TreeItemCollapsibleState,
+  ThemeIcon,
+} from "vscode";
+import { readFileSync } from "fs";
+import * as path from "path";
+
+export class NpmScriptsProvider implements TreeDataProvider<Script> {
+  constructor(private workspaceRoot = "") {}
+
+  getTreeItem(item: Script): TreeItem {
+    return item;
+  }
+
+  getChildren(item?: Script): Script[] {
+    return item ? [] : this.getScripts();
+  }
+
+  private getScripts(): Script[] {
+    const packageJsonPath = path.join(this.workspaceRoot, "package.json");
+    const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
+
+    return Object.entries<string>(packageJson.scripts).map<Script>(
+      ([label, script]) =>
+        new Script(label, script, TreeItemCollapsibleState.None)
+    );
+  }
+}
+
+class Script extends TreeItem {
+  constructor(
+    public readonly label: string,
+    private script: string,
+    public readonly collapsibleState: TreeItemCollapsibleState
+  ) {
+    super(label, collapsibleState);
+    this.tooltip = `${this.label}-${this.script}`;
+    this.description = this.script;
+    this.command = {
+      title: "Run NPM script",
+      command: "npm-scripts-nvm.runNpmScript",
+      arguments: [this.label],
+    };
+  }
+
+  iconPath = new ThemeIcon("debug-start");
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,56 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { accessSync } from "fs";
+import { NpmScriptsProvider } from "./NpmScriptsProvider";
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "npm-scripts-nvm" is now active!');
+  const rootPath =
+    vscode.workspace.workspaceFolders &&
+    vscode.workspace.workspaceFolders.length > 0
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : "";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('npm-scripts-nvm.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from npm-scripts-nvm!');
-	});
+  const pathExists = (p: string) => {
+    try {
+      accessSync(path.join(rootPath, p));
+    } catch {
+      return false;
+    }
+    return true;
+  };
 
-	context.subscriptions.push(disposable);
+  const buildScriptText = (script: string) => {
+    const scripts = [];
+
+    if (pathExists(".nvmrc")) {
+      scripts.push("nvm use");
+    }
+
+    if (pathExists("yarn.lock")) {
+      scripts.push(`yarn ${script}`);
+    } else {
+      scripts.push(`npm run ${script}`);
+    }
+
+    return scripts.join(" && ");
+  };
+
+  vscode.window.createTreeView("npm-scripts-nvm.npmScripts", {
+    treeDataProvider: new NpmScriptsProvider(rootPath),
+  });
+
+  const commandHandler = (script: string) => {
+    const terminal = vscode.window.createTerminal(script);
+    terminal.show();
+    terminal.sendText(buildScriptText(script));
+  };
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "npm-scripts-nvm.runNpmScript",
+      commandHandler
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
